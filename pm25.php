7f0c096da69f7dd52356e6b031bc411277f64e18
Nigelchiang <nigelchiang@yeah.net>
2015-11-16 07:50:15 +0800

    new function

--- pm25.php
+++ pm25.php
@@ -10,24 +10,28 @@
 header('Content-type:text/html;charset=utf-8');
 
 require("getToken.php");
+function getPM25()
+{
+    $param = array("city" => "shenyang", "key" => "baa7b2f4ce8cd00af3b96daa6bdbf2d3");
+    $paramString = http_build_query($param);
+    $url = "http://web.juhe.cn:8080/environment/air/cityair?" . $paramString;
+    $pm25 = json_decode(getToken($url), true);
 
-$param = array("city" => "shenyang", "key" => "baa7b2f4ce8cd00af3b96daa6bdbf2d3");
-$paramString = http_build_query($param);
-$url = "http://web.juhe.cn:8080/environment/air/cityair?" . $paramString;
-$pm25 = json_decode(getToken($url), true);
-
-if ($pm25) {
-    if ($pm25['error_code'] == '0') {
-        $date = $pm25['result'][0]['citynow']['date'];
-        $city = $pm25['result'][0]['citynow']['city'];
-        $AQI = $pm25['result'][0]['citynow']['AQI'];
-        $quality = $pm25['result'][0]['citynow']['quality'];
+    if ($pm25) {
+        if ($pm25['error_code'] == '0') {
+            $date = $pm25['result'][0]['citynow']['date'];
+            $city = $pm25['result'][0]['citynow']['city'];
+            $AQI = $pm25['result'][0]['citynow']['AQI'];
+            $quality = $pm25['result'][0]['citynow']['quality'];
+            echo $date . "\n" . $city . "\n" . $AQI . "\n" . $quality;
+        } else {
+            echo $pm25['error_code'] . ":" . $pm25['reason'];
+        }
     } else {
-        echo $pm25['error_code'] . ":" . $pm25['reason'];
+        echo "请求失败";
     }
-} else {
-    echo "请求失败";
 }
+
 
 echo "<table>
 <tcaption>$date</tcaption>
