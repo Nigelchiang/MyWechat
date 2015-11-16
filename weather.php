22bffe68cf64a8cdefd11d7cd7aca320563cdda0
Nigelchiang <nigelchiang@yeah.net>
2015-11-16 15:40:41 +0800

    enhance

--- weather.php
+++ weather.php
@@ -11,7 +11,7 @@ function getWeather($cityname)
     $weather = json_decode(gettoken($url), ture);
 
 //    print_r($weather);
-    if ($weather['resultcode' == '200']) {
+    if ($weather['resultcode'] == '200') {
         $wen = "当前温度：" . $weather['result']['sk']['temp'];//当前温度
         $fen = "当前风向风级：" . $weather['result']['sk']['wind_direction'] . "-" . $weather['result']['sk']['wind_strength'];//当前风向风级
         $city = "城市：" . $weather['result']['today']['city'];//城市
@@ -19,8 +19,8 @@ function getWeather($cityname)
         $wendu = "今日温度：" . $weather['result']['today']['temperature'];//温度
         $chuan = "穿衣指数" . $weather['result']['today']['dressing_advice'];//穿衣指数
 
-        return $dangweather = $city . "\n" . $riqi . "\n" . $wendu . "\n" . $wen . "\n" . $fen . "\n" . $chuan;
-    }else{
+        return $city . "\n" . $riqi . "\n" . $wendu . "\n" . $wen . "\n" . $fen . "\n" . $chuan;
+    } else {
         return $weather['resultcode'] . ": " . $weather['reason'];
     }
 }
