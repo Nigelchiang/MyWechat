ca046858f87e2962b94a72c893f0f2d915e9980f
Nigelchiang <nigelchiang@yeah.net>
2015-12-06 12:54:41 +0800

    change sdk

--- test.php
+++ test.php
@@ -17,7 +17,18 @@ $server->on('message', 'text', function ($message) {
     $params = array("key" => "08ad04b298923b29a203d0aca21a9779", "info" => $message->Content);
     $url .= http_build_query($params);
     $response = file_get_contents($url);
-    $data = json_decode($response);
+    $data     = json_decode($response);
+    //处理链接类请求
+    if ($data->{'code'} == 200000) {
+        return Message::make('news', function () {
+            global $data;
+
+            return array(
+                Message::make('news_item')->title($data->{'text'})->picUrl($data->{'url'}),
+            );
+        });
+    }
+
     return Message::make('text')->content($data->{'text'});
 });
 
