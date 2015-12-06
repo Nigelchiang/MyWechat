54ee5b857a04c08a5338394ddffb68954518883b
Nigelchiang <nigelchiang@yeah.net>
2015-12-06 15:28:08 +0800

    change sdk

--- test.php
+++ test.php
@@ -16,12 +16,13 @@ $server->on('event', 'subscribe', function ($event) {
 
     $news = Message::make('news')->items(function () {
         global $event;
-        Message::make('news_item')->title("{$event->FromUserName}你好~欢迎关注！")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2Fwelcome
-        .jpg');
-        Message::make('news_item')->title("『1』发送图片可以查询照片中人脸的年龄和性别信息")->PicUrl('http://233.weego.sinaapp.com/images/face.jpg');
-        Message::make('news_item')->title("『2』发送一张两人合影的照片可以计算两人的相似程度")->PicUrl('http://233.weego.sinaapp.com/images/mask.png');
-        Message::make('news_item')->title("『3』机智的图灵机器人陪你聊天解闷,还可以查天气查火车查航班…")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2F%E5%9B%9B%E5%85%AD%E7%BA%A7%E6%9F%A5%E5%88%86.jpg');
-        Message::make('news_item')->title("『4』四六级查分功能正在开发中，敬请期待~")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2Fbaymax.png');
+
+        return array(
+            Message::make('news_item')->title("{$event->FromUserName}你好~欢迎关注！")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2Fwelcome.jpg'),
+            Message::make('news_item')->title("『1』发送图片可以查询照片中人脸的年龄和性别信息")->PicUrl('http://233.weego.sinaapp.com/images/face.jpg'),
+            Message::make('news_item')->title("『2』发送一张两人合影的照片可以计算两人的相似程度")->PicUrl('http://233.weego.sinaapp.com/images/mask.png'),
+            Message::make('news_item')->title("『3』机智的图灵机器人陪你聊天解闷,还可以查天气查火车查航班…")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2F%E5%9B%9B%E5%85%AD%E7%BA%A7%E6%9F%A5%E5%88%86.jpg'),
+            Message::make('news_item')->title("『4』四六级查分功能正在开发中，敬请期待~")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2Fbaymax.png'));
     });
 
     return $news;
