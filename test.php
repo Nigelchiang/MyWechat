295465f48ebeefc6c3031e767217149b21d58a0a
Nigelchiang <nigelchiang@yeah.net>
2015-12-09 02:14:50 +0800

    curl

--- test.php
+++ test.php
@@ -42,7 +42,7 @@ $server->on('message', 'text', function ($message) use ($welcome) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
 
