6229cae38b04a43a5900470ac0135a63e2ebeaf9
Nigelchiang <nigelchiang@yeah.net>
2015-12-09 12:10:41 +0800

    change important

--- MS_FaceDetectResult.php
+++ MS_FaceDetectResult.php
@@ -11,7 +11,6 @@
 //sae_xhprof_start();
 //取出URL中的数据放到数组中
 parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $rectangle);
-var_dump($rectangle);
 //通过cUrl下载的图片，放到imagecreatefromstring里，就可以了
 $url = $_GET['$url'];
 $img = imagecreatefromstring(getImg($url));
