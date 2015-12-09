cb85662e1c66c9ac2322852ffa4f6b6a8cbf2e5f
Nigelchiang <nigelchiang@yeah.net>
2015-12-09 22:44:39 +0800

    reference

--- MS_Face_Detect.php
+++ MS_Face_Detect.php
@@ -78,7 +78,7 @@ function draw($url, &$rectangle) {
     //    $stor = new SaeStorage("n353jmy031","zwwkm3wjxmmkxkhwzlyjhxz3lh2xkyj3zhx014lh");
     //imagepng这样的函数不支持wrapper,用临时文件解决
     $bool = imagejpeg($img, SAE_TMP_PATH . $filename);//保存为临时文件
-    
+
     sae_log("保存的文件名：" . $filename);
     file_put_contents("saestor://wechatimg/$filename",
                       file_get_contents(SAE_TMP_PATH . $filename));
@@ -116,8 +116,9 @@ function drawRec(&$rec, &$img) {
     //画一个矩形
     $bool = imagerectangle($img, $x1, $y1, $x2, $y2, $color);
     //画点
+//    imagesetthickness($img, 8);
     foreach ($points as $point) {
-        $bool = imagerectangle($img, $point['x'] - 3, $point['y'] - 3, $point['x'] + 3, $point['y'] + 3, $color);
+        $bool = imagerectangle($img, $point['x'] - 5, $point['y'] - 5, $point['x'] + 5, $point['y'] + 5, $color);
     }
 
     if (!$bool) {
