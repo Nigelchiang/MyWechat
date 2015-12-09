746b77ef1181843ecbe2fd365e0fea0edc52cc6e
Nigelchiang <nigelchiang@yeah.net>
2015-12-09 19:14:20 +0800

    new

--- MS_Face_Detect.php
+++ MS_Face_Detect.php
@@ -68,8 +68,8 @@ function draw($url, $rectangle) {
     foreach ($rectangle as $rec) {
         drawRec($rec, $img);
     }
-    $filename = parse_url($url, PHP_URL_HOST);
-    imagejpeg($img, "saestor://wechatimg/$filename");
+    $filename = parse_url($url, PHP_URL_PATH);
+    imagejpeg($img, "saestor://wechatimg$filename");
     $stor = new SaeStorage();
 
     return $stor->getUrl("wehcatimg", $filename);
