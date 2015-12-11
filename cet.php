2254988db007419a42f5c74860426bc1a86407fa
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 09:31:22 +0800

    cet

--- cet.php
+++ cet.php
@@ -1,5 +1,5 @@
 <?php
-session_start();?>
+session_start(); ?>
 <!DOCTYPE html>
 <html lang="zh">
 <head>
@@ -13,17 +13,21 @@ session_start();?>
 $openid = $_GET['openid'];
 
 //查询数据库，openid是否已经存在
-$mysql = new SaeMysql();
-$query = "SELECT examid FROM cet WHERE openid=$openid";
-$examid=$mysql->getData($query);
+$mysql  = new SaeMysql();
+$query  = "SELECT examid FROM cet WHERE openid=$openid";
+$examid = $mysql->getData($query);
 //用户尚未备份考号
 if (empty($examid)) {
-    $signup ="INSERT INTO cet (openid) VALUES($openid) ";
-    $mysql->runSql($signup);
-    echo "你已成功注册"."<br/>";
+    $signup = "INSERT INTO cet (openid) VALUES($openid) ";
+    $bool   = $mysql->runSql($signup);
+    if (!$bool) {
+        echo "注册失败";
+    } else {
+        echo "你已成功注册" . "<br/>";
+    }
     //跳到填写考号和姓名的页面
     //带上openid或者设置session
-}else{
+} else {
     //跳到结果的页面 //带上openid或者session和
     //页面获取exanid和姓名直接查询，将查询结果存到数据库并显示到页面，生成一个模板图片，让用户保存
 }
