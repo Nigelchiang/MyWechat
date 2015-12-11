160ff3648750e58c4b0b733e7249360e5e488102
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 23:48:31 +0800

    cet

--- test.php
+++ test.php
@@ -28,12 +28,11 @@ $welcome = function ($user_name = null) {
     $i = 1;
 
     return array(
-        Message::make('news_item')->title("{$user_name} 你好~欢迎关注！")->PicUrl('http://n1gel-n1gel.stor.sinaapp
-        .com/img%2Fwelcome.jpg'),
+        Message::make('news_item')->title("{$user_name} 你好~欢迎关注！")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2Fwelcome.jpg'),
         Message::make('news_item')->title("『" . $i++ . "』发送图片可以查询照片中人脸的年龄和性别信息,还会在脸上标出来哦…")->PicUrl('http://n1gel-wechatimg.stor.sinaapp.com/mmbizaC7DypReicewYESlc5gXjH3IKQbYribnF72lBOIpmK0BWKZ6XTVdcSmaPzwp4NibAqdZTzSYuxNaRoqbrtqaacNWA0814814157.jpg'),
         Message::make('news_item')->title("『" . $i++ . "』机智的图灵机器人陪你聊天解闷,可以查天气查火车查航班…")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/2786001_213751420000_2.jpg'),
         Message::make('news_item')->title("『" . $i++ . "』新功能：语音聊天~直接给我发送语音就可以聊天了哦~")->PicUrl('http://www.36dsj.com/wp-content/uploads/2015/03/228.jpg'),
-        Message::make('news_item')->title("『" . $i++ . "』四六级查分已经上线,回复『46』来备份考号吧！")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2F%E5%9B%9B%E5%85%AD%E7%BA%A7%E6%9F%A5%E5%88%86.jpg'));
+        Message::make('news_item')->title("『" . $i++ . "』四六级查分已经上线,回复\"46\"来备份考号吧！")->PicUrl('http://n1gel-n1gel.stor.sinaapp.com/img%2F%E5%9B%9B%E5%85%AD%E7%BA%A7%E6%9F%A5%E5%88%86.jpg'));
 };
 $server->on('event', 'subscribe', function ($event) use ($welcome) {
     sae_log("用户关注: " . $event->openid);
@@ -74,7 +73,6 @@ $server->on('event', 'unsubscribe', function ($event) {
 
 //文字消息处理，调用图灵机器人
 $server->on('message', 'text', function ($message) use ($welcome) {
-    //    sae_log("消息内容 " . $message->Content);
     //四六级查分-备份考号
     return handleText($message->Content, $message->FromUserName, $welcome);
 });
@@ -128,7 +126,7 @@ echo $result;
 /**
  * 处理文字消息
  * @param $text    string
- * @param $openid string
+ * @param $openid  string
  * @param $welcome closure
  * @return mixed
  */
