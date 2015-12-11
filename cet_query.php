fba5fd68d19c535fd8b8ea62fed773fffa70e473
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 19:51:04 +0800

    cet

--- cet_query.php
+++ cet_query.php
@@ -13,3 +13,9 @@ sae_log("这里是查询界面，会有两个输入框和一个查询按钮\n" .
 //填完了之后会跳到结果界面
 sleep(2);
 header("Location:http://5.n1gel.sinaapp.com/cet_result.php");
+
+function sae_log($msg) {
+    sae_set_display_errors(false);//关闭信息输出
+    sae_debug($msg);//记录日志
+    sae_set_display_errors(true);//记录日志后再打开信息输出，否则会阻止正常的错误信息的显示
+}
--- cet_result.php
+++ cet_result.php
@@ -11,3 +11,10 @@ $examid = $_SESSION['examid'];
 $name   = $_SESSION['name'];
 echo "这里是成绩展示界面，会生成一张成绩的图片\n";
 echo "openid".$openid;
+
+
+function sae_log($msg) {
+    sae_set_display_errors(false);//关闭信息输出
+    sae_debug($msg);//记录日志
+    sae_set_display_errors(true);//记录日志后再打开信息输出，否则会阻止正常的错误信息的显示
+}
