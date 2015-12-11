424edc33303e2aab86b46bca913806344bcd0012
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 09:49:52 +0800

    cet

--- cet.php
+++ cet.php
@@ -21,9 +21,11 @@ if (empty($examid)) {
     $signup = "INSERT INTO cet (openid) VALUES('$openid') ";
     $bool   = $mysql->runSql($signup);
     if (!$bool) {
-        echo sprintf("注册失败 %d : %s",$mysql->errno(),$mysql->errmsg());
+        echo $debug=sprintf("注册失败 %d : %s",$mysql->errno(),$mysql->errmsg());
+        sae_log($debug);
     } else {
         echo "你已成功注册" . "<br/>";
+        sae_log("成功注册");
     }
     //跳到填写考号和姓名的页面
     //带上openid或者设置session
@@ -33,6 +35,11 @@ if (empty($examid)) {
 }
 
 $mysql->closeDb();
+function sae_log($msg) {
+    sae_set_display_errors(false);//关闭信息输出
+    sae_debug($msg);//记录日志
+    sae_set_display_errors(true);//记录日志后再打开信息输出，否则会阻止正常的错误信息的显示
+}
 ?>
 </body>
 </html>
