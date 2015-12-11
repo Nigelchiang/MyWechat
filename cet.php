af992b887e884ab2a9a49e9c71de0f675c6d65ea
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 10:24:05 +0800

    cet

--- cet.php
+++ cet.php
@@ -14,30 +14,33 @@
 $openid = $_GET['openid'];
 
 //查询数据库，openid是否已经存在
-$mysql  = new SaeMysql();
-$query  = "SELECT openid FROM cet WHERE openid='$openid'";
-$openid = $mysql->getVar($query);
+$mysql = new SaeMysql();
+$query = "SELECT openid,examid FROM cet WHERE openid='$openid'";
+list($openid, $examid) = $mysql->getLine($query);
 //var_dump($examid);
 //var_dump($mysql->getLine($query));
 //var_dump($mysql->getVar($query));
-sae_log(json_encode($openid));
+sae_log(json_encode($openid . " " . $examid));
 //用户尚未备份考号
 if (empty($openid)) {
     $signup = "INSERT INTO cet (openid) VALUES('$openid') ";
     $bool   = $mysql->runSql($signup);
     if (!$bool) {
-        echo $debug=sprintf("注册失败 %d : %s",$mysql->errno(),$mysql->errmsg());
+        echo $debug = sprintf("注册失败 %d : %s", $mysql->errno(), $mysql->errmsg());
         sae_log($debug);
     } else {
-        echo "你已成功注册";
+        echo "你已成功注册" . "<br/>";
         sae_log("成功注册");
+        echo "即将跳转到备份的页面";
         //跳到填写考号和姓名的页面
         //带上openid或者设置session
     }
-} else {
-    echo "难道跳到这里来了？";
+} elseif (empty($examid)) {
+    echo "即将跳转到备份的页面";
     //跳到备份的页面 //带上openid或者session和
     //页面获取exanid和姓名直接查询，将查询结果存到数据库并显示到页面，生成一个模板图片，让用户保存
+} else {
+    echo "即将跳转到成绩结果界面";
 }
 
 $mysql->closeDb();
@@ -46,6 +49,7 @@ function sae_log($msg) {
     sae_debug($msg);//记录日志
     sae_set_display_errors(true);//记录日志后再打开信息输出，否则会阻止正常的错误信息的显示
 }
+
 ?>
 </body>
 </html>
