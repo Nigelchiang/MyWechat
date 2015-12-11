81a36a282333496de19fc7ae81283a84cb5ccf0c
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 23:10:20 +0800

    cet

--- cet_query.php
+++ cet_query.php
@@ -8,54 +8,58 @@ $name   = $_POST['name'];
 $examidErr = $nameErr = '';
 $is_examid = $is_name = false;
 //region 输入验证
-if (empty($examid)) {
-    $examidErr = "请输入15位准考证号";
-    ?>
-    <script>
-        alert("<?php echo $examidErr?>");
-        document.getElementById("examid").focus();
-    </script>
-    <?php
-} elseif (!test_examid($examid)) {
-    $examidErr = "准考证号为15位数字";
-    ?>
-    <script>
-        alert("<?php echo $examidErr?>");
-        document.getElementById("examid").focus();
-    </script>
-    <?php
-} else {
-    $is_examid = true;
-}
-if (empty($name)) {
-    $nameErr = "名字未填写";
-    ?>
-    <script>
-        alert("<?php echo $nameErr?>");
-    </script>
-    <?php
-} elseif (!test_name($name)) {
-    $nameErr = "姓名为2~3个汉字";
-    ?>
-    <script>
-        alert("<?php echo $nameErr?>");
-    </script>
-    <?php
-} else {
-    $is_name = true;
-}
-if ($is_examid && $is_name) {
-    $_SESSION['examid'] = $examid;
-    $_SESSION['name']   = $name;
+//防止第一次进入就报错
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($examid)) {
+        $examidErr = "请输入15位准考证号";
+        ?>
+        <script>
+            alert("<?php echo $examidErr?>");
+            document.getElementById("examid").focus();
+        </script>
+        <?php
+    } elseif (!test_examid($examid)) {
+        $examidErr = "准考证号为15位数字";
+        ?>
+        <script>
+            alert("<?php echo $examidErr?>");
+            document.getElementById("examid").focus();
+        </script>
+        <?php
+    } else {
+        $is_examid = true;
+    }
+    if (empty($name)) {
+        $nameErr = "名字未填写";
+        ?>
+        <script>
+            alert("<?php echo $nameErr?>");
+        </script>
+        <?php
+    } elseif (!test_name($name)) {
+        $nameErr = "姓名为2~3个汉字";
+        ?>
+        <script>
+            alert("<?php echo $nameErr?>");
+        </script>
+        <?php
+    } else {
+        $is_name = true;
+    }
 
-    //更新数据库
-    $mysql  = new SaeMysql();
-    $update = "insert into cet (examid,name,openid) VALUES ('$examid','$name','$openid')";
-    $mysql->runSql($update);
-    sae_log("插入成绩出错" . $mysql->errmsg());
-    $mysql->closeDb();
-    //跳转
-    header("Location:http://5.n1gel.sinaapp.com/cet_result.php");
+    if ($is_examid && $is_name) {
+        $_SESSION['examid'] = $examid;
+        $_SESSION['name']   = $name;
+
+        //更新数据库
+        $mysql  = new SaeMysql();
+        $update = "insert into cet (examid,name,openid) VALUES ('$examid','$name','$openid')";
+        $mysql->runSql($update);
+        sae_log("插入成绩出错" . $mysql->errmsg());
+        $mysql->closeDb();
+        //跳转
+        header("Location:http://5.n1gel.sinaapp.com/cet_result.php");
+    }
 }
 //endregion
 ?>
@@ -77,18 +81,23 @@ if ($is_examid && $is_name) {
         </div>
         <div class="form-group">
             <div class="input-group">
-                <div class="input-group-addon">准考证号</div>
-                <input type="text" maxlength="15" name="examid" class="form-control" placeholder="姓名只输入前3个字">
+                <div class="input-group-addon">考号</div>
+                <input type="text" maxlength="15" name="examid" class="form-control" placeholder="请输入15位准考证号">
             </div>
         </div>
         <div class="form-group">
             <div class="input-group">
                 <div class="input-group-addon">姓名</div>
-                <input type="text" maxlength="15" name="name" class="form-control" placeholder="请输入15位准考证号">
+                <input type="text" maxlength="3" name="name" class="form-control" placeholder="姓名只输入前3个字">
             </div>
         </div>
-        <button type="button" class="btn btn-primary btn-block">备份</button>
+        <button type="button" class="btn btn-primary btn-block" id="submit" onclick="onSubmit();">备份</button>
     </form>
+    <script>
+        function onSubmit() {
+            document.getElementById('submit').submit();
+        }
+    </script>
     </body>
     </html>
 <?php
