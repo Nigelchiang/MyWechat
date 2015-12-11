589185028a81cf5ad993f7c8762a8047c2a2cecf
Nigelchiang <nigelchiang@yeah.net>
2015-12-11 19:50:23 +0800

    cet

--- cet_query.php
+++ cet_query.php
@@ -8,8 +8,7 @@
 session_start();
 $openid = $_SESSION['openid'];
 
-echo "这里是查询界面，会有两个输入框和一个查询按钮\n";
-echo "openid:" . $openid;
+sae_log("这里是查询界面，会有两个输入框和一个查询按钮\n" . "openid:" . $openid);
 
 //填完了之后会跳到结果界面
 sleep(2);
